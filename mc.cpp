16d089a9a338727fe95109a9f0595f1687042191
Stephan Heßelmann <hesselmann@physik.rwth-aachen.de>
2015-04-02 16:40:36 +0200

    update

--- mc.cpp
+++ mc.cpp
@@ -369,6 +369,7 @@ void CLASSNAME::do_update()
 			if (configSpace.AddRandomVertices<N>(preFactor, false))
 			{
 				acceptedUpdates(UpdateType::AddVertex, state) += 1.0;
+				++rebuildCnt;
 			}
 			proposedUpdates(UpdateType::AddVertex, state) += 1.0;
 		}
@@ -379,6 +380,7 @@ void CLASSNAME::do_update()
 			if (configSpace.RemoveRandomVertices<N>(preFactor, false))
 			{
 				acceptedUpdates(UpdateType::RemoveVertex, state) += 1.0;
+				++rebuildCnt;
 			}
 			proposedUpdates(UpdateType::RemoveVertex, state) += 1.0;
 		}
@@ -389,6 +391,7 @@ void CLASSNAME::do_update()
 			if (configSpace.AddRandomVertices<N>(preFactor, false))
 			{
 				acceptedUpdates(UpdateType::Add2Vertices, state) += 1.0;
+				++rebuildCnt;
 			}
 			proposedUpdates(UpdateType::Add2Vertices, state) += 1.0;
 		}
